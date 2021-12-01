9bd98f49a29cd3811a12e0ced5745445c8b87d56
Yoann Prot <yoann.prot@doctolib.com>
2021-12-01 14:20:18 +0100

    :construction_worker: fix ci

--- test/advent_of_code/day1/day1_test.rb
+++ test/advent_of_code/day1/day1_test.rb
@@ -6,42 +6,42 @@ module AdventOfCode
   class Day1Test < Minitest::Test
 
     def test_no_data
-      assert_equal 0, day1([])
-      assert_equal 0, day1_bis([])
+      assert_equal 0, day1_simple([])
+      assert_equal 0, day1_complex([])
     end
 
     def test_one_data
-      assert_equal 0, day1([10])
-      assert_equal 0, day1_bis([10])
+      assert_equal 0, day1_simple([10])
+      assert_equal 0, day1_complex([10])
     end
 
     def test_only_up
-      assert_equal 2, day1([1, 2, 3])
-      assert_equal 0, day1_bis([1, 2, 3])
+      assert_equal 2, day1_simple([1, 2, 3])
+      assert_equal 0, day1_complex([1, 2, 3])
     end
 
     def test_only_down
-      assert_equal 0, day1([3, 2, 1])
-      assert_equal 0, day1_bis([3, 2, 1])
+      assert_equal 0, day1_simple([3, 2, 1])
+      assert_equal 0, day1_complex([3, 2, 1])
     end
 
     def test_mixed
-      assert_equal 4, day1([1, 20, 25, 24, 30, 31, 5])
-      assert_equal 3, day1_bis([1, 2, 3, 4, 5, 0, 5])
+      assert_equal 4, day1_simple([1, 20, 25, 24, 30, 31, 5])
+      assert_equal 3, day1_complex([1, 2, 3, 4, 5, 0, 5])
     end
 
     def test_final
-      lines = File.open('/Users/yoann.prot/dev/advent-of-code-2021/test/advent_of_code/day1/input.txt').readlines
+      values = File.open('./test/advent_of_code/day1/input.txt').readlines.map{|v| v.to_i}
 
-      assert_equal 1215, day1(lines.map{|v| v.to_i})
-      assert_equal 1215, day1_bis(lines.map{|v| v.to_i})
+      assert_equal 1215, day1_simple(values)
+      assert_equal 1150, day1_complex(values)
     end
 
-    def day1(input)
+    def day1_simple(input)
       AdventOfCode::Day1.simple_measure(input)
     end
 
-    def day1_bis(input)
+    def day1_complex(input)
       AdventOfCode::Day1.complex_measure(input)
     end
 
