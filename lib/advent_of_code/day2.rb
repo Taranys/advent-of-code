04b85d9be97ea561504016285c412f61bec2a259
Yoann Prot <yoann.prot@doctolib.com>
2021-12-02 09:03:49 +0100

    :sparkles: working version !

--- lib/advent_of_code/day2.rb
+++ lib/advent_of_code/day2.rb
@@ -1,10 +1,11 @@
 module AdventOfCode
   class Day2
-    attr_accessor :horizontal, :depth
+    attr_accessor :horizontal, :depth, :aim
 
     def initialize
       @horizontal = 0
       @depth = 0
+      @aim = 0
     end
 
     def move(input)
@@ -18,6 +19,24 @@ module AdventOfCode
           @depth = @depth + value.to_i
         when "up"
           @depth = @depth - value.to_i
+        end
+      end
+
+      self
+    end
+
+    def move_with_aim(input)
+      input.each do |line|
+        direction, value = line.split
+
+        case direction
+        when "forward"
+          @horizontal = @horizontal + value.to_i
+          @depth = @depth + value.to_i * @aim
+        when "down"
+          @aim = @aim + value.to_i
+        when "up"
+          @aim = @aim - value.to_i
         end
       end
 
--- test/advent_of_code/day2/day2_test.rb
+++ test/advent_of_code/day2/day2_test.rb
@@ -9,24 +9,56 @@ module AdventOfCode
   class Day2Test < Minitest::Test
     def test_no_data
       assert_equal 0, create_submarine.move([]).horizontal
+
+      assert_equal 0, create_submarine.move_with_aim([]).horizontal
     end
 
     def test_forward
       assert_equal 10, create_submarine.move(["forward 10"]).horizontal
+
+      assert_equal 5, create_submarine.move_with_aim(["forward 5"]).horizontal
     end
 
     def test_down
       assert_equal 10, create_submarine.move(["down 10"]).depth
+
+      submarine = create_submarine.move_with_aim(["down 5"])
+      assert_equal 0, submarine.depth
+      assert_equal 5, submarine.aim
     end
 
     def test_up
       assert_equal 5, create_submarine.move(["down 8", "up 3"]).depth
+
+      submarine = create_submarine.move_with_aim(["down 12", "up 3"])
+      assert_equal 0, submarine.depth
+      assert_equal 9, submarine.aim
+    end
+
+    def test_forward_with_aim
+      submarine = create_submarine.move_with_aim(["down 2", "forward 5"])
+      assert_equal 5, submarine.horizontal
+      assert_equal 2, submarine.aim
+      assert_equal 10, submarine.depth
+    end
+
+    def test_example
+      submarine = create_submarine.move_with_aim(["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"])
+      assert_equal 15, submarine.horizontal
+      assert_equal 10, submarine.aim
+      assert_equal 60, submarine.depth
     end
 
     def test_final
       submarine = create_submarine.move(AdventOfCode::Helper.loadInput(2))
 
       assert_equal 1561344, submarine.depth * submarine.horizontal
+    end
+
+    def test_final_with_aim
+      submarine = create_submarine.move_with_aim(AdventOfCode::Helper.loadInput(2))
+
+      assert_equal 1848454425, submarine.depth * submarine.horizontal
     end
 
     def create_submarine
