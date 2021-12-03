28ae3dbdf6ab6f582cc40ec0af6e04028bc9531c
Yoann Prot <yoann.prot@doctolib.com>
2021-12-03 10:32:13 +0100

    :sparkle: working version

--- lib/advent_of_code/day3.rb
+++ lib/advent_of_code/day3.rb
@@ -4,14 +4,45 @@ module AdventOfCode
   # Day 3
   class Day3
     def self.power_consumption(input)
-      gamma = input.map(&:strip)
-          .map { |entry| entry.each_char.map(&:to_i) }
-         .transpose
+      gamma = input.transpose
          .map { |values| values.count(0) }
          .map { |number_of_0| number_of_0 >= (input.count / 2) ? 0 : 1 }
          .join
 
       [gamma.to_i(2), gamma.chars.map{ |v| v == '0' ? '1' : '0' }.join.to_i(2)]
+    end
+
+    def self.bit_criteria(input, position)
+      number_of_0 = input.count { |value| value[position] == '0' }
+      return 1 if number_of_0 == (input.count / 2)
+      number_of_0 >= (input.count / 2) ? 0 : 1
+    end
+
+    def self.oxygen_generator_support(input)
+      result = ''
+
+      for i in 0..input[0].size-1
+        p input
+        bc = bit_criteria(input, i).to_s
+        result += bc
+        input = input.select{ |value| value[i] == bc }
+      end
+
+      result.to_i(2)
+    end
+
+    def self.c02(input)
+      result = ''
+
+      for i in 0..input[0].size-1
+        p input
+        return input[0].to_i(2) if input.size == 1
+        bc = (bit_criteria(input, i) == 0 ? 1 : 0).to_s
+        result += bc
+        input = input.select{ |value| value[i] == bc }
+      end
+
+      result.to_i(2)
     end
   end
 end
--- test/advent_of_code/day3/day3_test.rb
+++ test/advent_of_code/day3/day3_test.rb
@@ -10,15 +10,37 @@ module AdventOfCode
     def test_example
       input = %w[00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010]
 
-      gamma, epsilon = AdventOfCode::Day3.power_consumption(input)
+      gamma, epsilon = AdventOfCode::Day3.power_consumption(normalize_data(input))
       assert_equal 22*9, gamma * epsilon
     end
 
     def test_final
       input = AdventOfCode::Helper.load_input(3)
 
-      gamma, epsilon = AdventOfCode::Day3.power_consumption(input)
+      gamma, epsilon = AdventOfCode::Day3.power_consumption(normalize_data(input))
       assert_equal 2972336, gamma * epsilon
+    end
+
+    def test_oxygen
+      input = %w[00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010]
+
+      assert_equal 23, AdventOfCode::Day3.oxygen_generator_support(input)
+    end
+
+    def test_c02
+      input = %w[00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010]
+
+      assert_equal 10, AdventOfCode::Day3.c02(input)
+    end
+
+    def test_final2
+      input = AdventOfCode::Helper.load_input(3).map(&:strip)
+
+      assert_equal 3368358, AdventOfCode::Day3.oxygen_generator_support(input) * AdventOfCode::Day3.c02(input)
+    end
+
+    def normalize_data(input)
+      input.map(&:strip).map { |entry| entry.each_char.map(&:to_i) }
     end
   end
 end
