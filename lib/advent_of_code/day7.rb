cd9225aea2a4e87c5bc226f9f2b2b5a62c3f9cf8
Yoann Prot <yoann.prot@doctolib.com>
2021-12-06 16:45:03 +0100

    :tada: prepare day 7

---
+++ lib/advent_of_code/day7.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  # Day 7
+  class Day7
+
+  end
+end
---
+++ test/advent_of_code/day7/day7_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+require_relative "../helper"
+require_relative "../../../lib/advent_of_code/day7"
+
+module AdventOfCode
+  class Day7Test < Minitest::Test
+    EXAMPLE = %(
+).split("\n").map(&:strip).reject(&:empty?)
+
+    def test_example
+    end
+
+    def test_final
+    end
+  end
+end
