551261c19acdea1b76fc074e1c4c8a995a3bdb29
Yoann Prot <yoann.prot@doctolib.com>
2021-12-07 10:08:22 +0100

    :sparkles: working second exercice

--- lib/advent_of_code/day7.rb
+++ lib/advent_of_code/day7.rb
@@ -3,12 +3,16 @@
 module AdventOfCode
   # Day 7
   class Day7
-    def self.align_crabs(positions)
-      positions.map.with_index { |i| coast_to_move_all_crab(positions, i) }.min
+    def self.align_crabs(positions, type)
+      positions.map { |i| coast_to_move_all_crab(positions, i, type) }.min
     end
 
-    def self.coast_to_move_all_crab(positions, i)
-      positions.map { |pos| (pos - i).abs }.sum
+    def self.coast_to_move_all_crab(positions, index, type)
+      if type == :simple
+        positions.map { |pos| (pos - index).abs }.sum
+      else
+        positions.map { |pos| ((pos - (index + 1)).abs + 1).times.sum }.sum
+      end
     end
   end
 end
--- test/advent_of_code/day7/day7_test.rb
+++ test/advent_of_code/day7/day7_test.rb
@@ -12,11 +12,13 @@ module AdventOfCode
 ).split("\n").map(&:strip).reject(&:empty?)
 
     def test_example
-      assert_equal 37, Day7.align_crabs(EXAMPLE.first.split(",").map(&:to_i))
+      assert_equal 37, Day7.align_crabs(EXAMPLE.first.split(",").map(&:to_i), :simple)
+      assert_equal 168, Day7.align_crabs(EXAMPLE.first.split(",").map(&:to_i), :complex)
     end
 
     def test_final
-      assert_equal 352997, Day7.align_crabs(Helper.load_input(7).first.split(",").map(&:to_i))
+      assert_equal 352_997, Day7.align_crabs(Helper.load_input(7).first.split(",").map(&:to_i), :simple)
+      assert_equal 101_571_302, Day7.align_crabs(Helper.load_input(7).first.split(",").map(&:to_i), :complex)
     end
   end
 end
