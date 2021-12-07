1362c8ea1d909394b8190d99a2ad272df31b8e23
Yoann Prot <yoann.prot@doctolib.com>
2021-12-07 09:31:06 +0100

    :sparkles: forst exercise

--- lib/advent_of_code/day7.rb
+++ lib/advent_of_code/day7.rb
@@ -3,6 +3,12 @@
 module AdventOfCode
   # Day 7
   class Day7
+    def self.align_crabs(positions)
+      positions.map.with_index { |i| coast_to_move_all_crab(positions, i) }.min
+    end
 
+    def self.coast_to_move_all_crab(positions, i)
+      positions.map { |pos| (pos - i).abs }.sum
+    end
   end
 end
--- test/advent_of_code/day7/day7_test.rb
+++ test/advent_of_code/day7/day7_test.rb
@@ -8,12 +8,15 @@ require_relative "../../../lib/advent_of_code/day7"
 module AdventOfCode
   class Day7Test < Minitest::Test
     EXAMPLE = %(
+    16,1,2,0,4,2,7,1,2,14
 ).split("\n").map(&:strip).reject(&:empty?)
 
     def test_example
+      assert_equal 37, Day7.align_crabs(EXAMPLE.first.split(",").map(&:to_i))
     end
 
     def test_final
+      assert_equal 352997, Day7.align_crabs(Helper.load_input(7).first.split(",").map(&:to_i))
     end
   end
 end
