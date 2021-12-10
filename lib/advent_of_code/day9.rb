42fbd4e24525ed482507126ade48724e22db4aa9
Yoann Prot <Taranys@users.noreply.github.com>
2021-12-10 09:30:14 +0100

    Day 9 (#8)
    * :sparkles: day 9 part 1
    
    * :recycle: refactoring of the code to prepare part 2
    
    * :sparkles: day 9 part 2
    
    * :recycle: refactoring
    
    * fix test

---
+++ lib/advent_of_code/day9.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  # SmokeBasin
+  class SmokeBasin
+    def initialize(input)
+      @map = input.map.with_index { |line, x| line.chars.map.with_index { |v, y| Point.new(v.to_i, x, y) } }
+
+      find_low_points
+      low_points.each { |point| define_basin(point) }
+    end
+
+    def low_points
+      @map.flatten.filter(&:low)
+    end
+
+    def basins
+      @map.flatten.group_by(&:basin_core).to_a.map { |group| group[0].nil? ? 0 : group[1].size }
+    end
+
+    private
+
+    def find_low_points
+      @map.each_with_index do |line, row|
+        line.each_with_index do |_, column|
+          current = @map[row][column]
+          current.low = true if adjacent_points(current.coordinate).all? { |adj| adj.risk_level > current.risk_level }
+        end
+      end
+    end
+
+    def define_basin(point)
+      adjacent_points(point.coordinate).each do |adj|
+        next unless adj.basin_core.nil?
+        next if adj.risk_level == 10
+
+        adj.basin_core = point.basin_core
+        define_basin(adj)
+      end
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    def adjacent_points(coordinate)
+      row, column = coordinate
+      adjacents = []
+
+      adjacents << @map[row][column - 1] if column.positive?
+      adjacents << @map[row][column + 1] unless @map[row][column + 1].nil?
+      adjacents << @map[row - 1][column] if row.positive?
+      adjacents << @map[row + 1][column] unless @map[row + 1].nil?
+
+      adjacents
+    end
+    # rubocop:enable Metrics/AbcSize
+  end
+
+  # Point
+  class Point
+    attr_reader :low
+    attr_accessor :basin_core
+
+    def initialize(value, row, column)
+      @value = value
+      @row = row
+      @column = column
+      @basin_core = nil
+    end
+
+    def low=(value)
+      @low = value
+      @basin_core = self
+    end
+
+    def risk_level
+      @value + 1
+    end
+
+    def coordinate
+      [@row, @column]
+    end
+  end
+end
---
+++ test/advent_of_code/day9/day9_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+require_relative "../helper"
+require_relative "../../../lib/advent_of_code/day9"
+
+module AdventOfCode
+  class Day9Test < Minitest::Test
+    EXAMPLE = %(
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+).split("\n").map(&:strip).reject(&:empty?)
+    def test_example
+      basin = SmokeBasin.new(EXAMPLE)
+      assert 15, basin.low_points.map(&:risk_level).sum
+      assert 1134, basin.basins.sort { |a, b| a <=> b }.take(3).inject(:*)
+    end
+
+    def test_final
+      basin = SmokeBasin.new(Helper.load_input(9))
+      assert_equal 539, basin.low_points.map(&:risk_level).sum
+      assert_equal 736_920, basin.basins.sort { |a, b| -(a <=> b) }.take(3).inject(:*)
+    end
+  end
+end
