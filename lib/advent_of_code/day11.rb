d959ae31aa5a7cfbe6b553abb87823b36971ff74
Yoann Prot <yoann.prot@doctolib.com>
2021-12-11 17:20:43 +0100

    :construction: day 11 part 1

---
+++ lib/advent_of_code/day11.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  class Cave
+    def initialize(input)
+      @map = input.map { |line| line.chars.map { |light_value| Octopus.new(light_value) } }
+
+      @map.each_with_index do |line, i|
+        line.each_with_index do |current, j|
+          current.adjacents = find_adjacents(@map, i, j)
+        end
+      end
+    end
+
+    def steps(value)
+      value.times do |step|
+        increase_octopuses
+        p step if @map.flatten.all? { |octopus| octopus.light_value >= 10 }
+        clean_up
+      end
+
+      self
+    end
+
+    def increase_octopuses
+      @map.flatten.map(&:increase_light)
+      # @map.each_with_index do |line, i|
+      #   line.each_with_index do |current, j|
+      #     current.increase_light
+      #   end
+      # end
+    end
+
+    def clean_up
+      @map.flatten.each { |octopus| octopus.clean_up }
+      # @map.each_with_index do |line, i|
+      #   line.each_with_index do |current, j|
+      #     current.clean_up
+      #   end
+      # end
+    end
+
+    def octopuses
+      @map.flatten
+    end
+
+    def find_adjacents(map, i, j)
+      result = []
+
+      result << map[i-1][j-1] if i > 0 && j > 0
+      result << map[i-1][j] if i > 0
+      result << map[i-1][j+1] if i > 0 && j < (map[0].size - 1)
+
+      result << map[i][j-1] if j > 0
+      result << map[i][j+1] if j < (map[0].size - 1)
+
+      result << map[i+1][j-1] if i < (map.size - 1) && j > 0
+      result << map[i+1][j] if i < (map.size - 1)
+      result << map[i+1][j+1] if i < (map.size - 1) && j < (map[0].size - 1)
+      result
+    end
+  end
+
+  class Octopus
+    attr_reader :flash_count, :light_value
+    attr_accessor :adjacents
+
+    def initialize(light_value)
+      @light_value = light_value.to_i
+      @flash_count = 0
+      @adjacents = []
+    end
+
+    def increase_light
+      @light_value += 1
+      if @light_value == 10
+        @adjacents.each(&:increase_light)
+      end
+    end
+
+    def clean_up
+      if @light_value >= 10
+        @flash_count += 1
+        @light_value = 0
+      end
+    end
+  end
+end
---
+++ test/advent_of_code/day11/day11_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+require_relative "../helper"
+require_relative "../../../lib/advent_of_code/day11"
+
+module AdventOfCode
+  class Day10Test < Minitest::Test
+    EXAMPLE = """
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+""".split("\n").map(&:strip).reject(&:empty?)
+
+    def test_example
+      assert_equal 1656, Cave.new(EXAMPLE).steps(100).octopuses.map(&:flash_count).sum
+      assert_equal 0, Cave.new(EXAMPLE).steps(200).octopuses.map(&:flash_count).sum
+    end
+
+    def test_final
+      assert_equal 1713, Cave.new(Helper.load_input(11)).steps(100).octopuses.map(&:flash_count).sum
+      assert_equal 1713, Cave.new(Helper.load_input(11)).steps(300).octopuses.map(&:flash_count).sum
+    end
+  end
+end
