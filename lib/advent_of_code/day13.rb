68c8c7540dea0a680b991c7364ebd5d9a55949ff
Yoann Prot <yoann.prot@doctolib.com>
2021-12-13 16:14:37 +0100

    :recycle: improve code lisibility

--- lib/advent_of_code/day13.rb
+++ lib/advent_of_code/day13.rb
@@ -14,56 +14,71 @@ module AdventOfCode
           @dots << line.split(",").map(&:to_i)
         end
       end
+
+      @map = create_map
     end
 
     def visible_dots_after_one_fold
-      right = @dots.map { |val| val[0] }.max + 1
-      down = @dots.map { |val| val[1] }.max + 1
-
-      map = Array.new(down) { Array.new(right, false) }
-      @dots.each { |dot| map[dot[1]][dot[0]] = true }
-
-      fold(map, @folds.first)
-
-      map = map.reject { |line| line.none? { |dot| dot == true } }
-
-      map.map { |line| line.map { |dot| dot ? 1 : 0 }.sum }.sum
+      fold(@folds.first)
+      remove_empty_lines
+      count_dots
     end
 
     def display_folded_dots
-      right = @dots.map { |val| val[0] }.max + 1
-      down = @dots.map { |val| val[1] }.max + 1
-
-      map = Array.new(down) { Array.new(right, false) }
-      @dots.each { |dot| map[dot[1]][dot[0]] = true }
-
-      @folds.each { |fold_position| fold(map, fold_position) }
-
-      map = map.reject { |line| line.none? { |dot| dot == true } }
-
-      map.map { |line| line.map { |dot| dot ? "#" : "." }.join }.join("\n")
+      @folds.each { |fold_position| fold(fold_position) }
+      remove_empty_lines
+      map_to_s
     end
 
     private
 
-    def fold(map, fold_line)
-      type, index = fold_line[0], fold_line[1].to_i
+    def create_map
+      right = @dots.map { |val| val[0] }.max + 1
+      down = @dots.map { |val| val[1] }.max + 1
 
-      map.each_with_index do |line, row|
-        line.each_with_index do |dot, column|
-          if type == "y" && row > index && dot
-            map[row][column] = false
-            map[index - (row - index)][column] = true
-          end
+      map = Array.new(down) { Array.new(right, false) }
+      @dots.each { |dot| map[dot[1]][dot[0]] = true }
+      map
+    end
 
-          if type == "x" && column > index && dot
-            map[row][column] = false
-            map[row][index - (column - index)] = true
-          end
+    def remove_empty_lines
+      @map.reject! { |line| line.none? { |dot| dot == true } }
+    end
+
+    def count_dots
+      @map.map { |line| line.map { |dot| dot ? 1 : 0 }.sum }.sum
+    end
+
+    def map_to_s
+      @map.map { |line| line.map { |dot| dot ? "#" : " " }.join.rstrip }.join("\n")
+    end
+
+    def fold(fold_line)
+      type = fold_line[0]
+      index = fold_line[1].to_i
+
+      @map.each_with_index do |line, row|
+        line.each_with_index do |_dot, column|
+          swap_dot_vertically(index, row, column) if type == "y"
+          swap_dot_horizontally(index, row, column) if type == "x"
         end
       end
 
-      map
+      @map
+    end
+
+    def swap_dot_horizontally(index, row, column)
+      return unless column > index && @map[row][column]
+
+      @map[row][column] = false
+      @map[row][index - (column - index)] = true
+    end
+
+    def swap_dot_vertically(index, row, column)
+      return unless row > index && @map[row][column]
+
+      @map[row][column] = false
+      @map[index - (row - index)][column] = true
     end
   end
 end
