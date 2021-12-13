221ffc49eebfe1086635b92235e517412eca61cd
Yoann Prot <Taranys@users.noreply.github.com>
2021-12-13 16:49:07 +0100

    Day 11 (#10)
    * :construction: day 11 part 1
    
    * part 2
    
    * :sparkles: day 11 !
    
    * :construction: improvments
    
    * :mute: remove linter error

---
+++ lib/advent_of_code/day11.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  # Cave
+  class Cave
+    attr_reader :octopuses
+
+    def initialize(input)
+      @octopuses = []
+
+      map = input.map { |line| line.chars.map { |light_value| Octopus.new(light_value) } }
+
+      map.each_with_index do |line, i|
+        line.each_with_index do |current, j|
+          current.adjacents = find_adjacents(map, i, j)
+          @octopuses << current
+        end
+      end
+    end
+
+    def steps(value)
+      value.times do
+        @octopuses.each(&:increase_light)
+        @octopuses.each(&:clean_up)
+      end
+
+      self
+    end
+
+    def synchronized_step
+      10_000_000.times do |step|
+        @octopuses.each(&:increase_light)
+        @octopuses.each(&:clean_up)
+        return (step + 1) if @octopuses.all? { |octopus| octopus.light_value.zero? }
+      end
+    end
+
+    private
+
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    def find_adjacents(map, row, column)
+      result = []
+
+      result << map[row - 1][column - 1] if row.positive? && column.positive?
+      result << map[row - 1][column] if row.positive?
+      result << map[row - 1][column + 1] if row.positive? && column < (map[0].size - 1)
+
+      result << map[row][column - 1] if column.positive?
+      result << map[row][column + 1] if column < (map[0].size - 1)
+
+      result << map[row + 1][column - 1] if row < (map.size - 1) && column.positive?
+      result << map[row + 1][column] if row < (map.size - 1)
+      result << map[row + 1][column + 1] if row < (map.size - 1) && column < (map[0].size - 1)
+
+      result
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  end
+
+  # Octopus
+  class Octopus
+    attr_reader :flash_count, :light_value
+    attr_accessor :adjacents
+
+    def initialize(light_value)
+      @light_value = light_value.to_i
+      @flash_count = 0
+      @adjacents = []
+    end
+
+    def increase_light
+      @light_value += 1
+      @adjacents.each(&:increase_light) if @light_value == 10
+    end
+
+    def clean_up
+      return unless @light_value >= 10
+
+      @flash_count += 1
+      @light_value = 0
+    end
+  end
+end
---
+++ test/advent_of_code/day11/day11_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+require_relative "../helper"
+require_relative "../../../lib/advent_of_code/day11"
+
+module AdventOfCode
+  class Day11Test < Minitest::Test
+    EXAMPLE = '
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+'.split("\n").map(&:strip).reject(&:empty?)
+
+    def test_example
+      assert_equal 1656, Cave.new(EXAMPLE).steps(100).octopuses.map(&:flash_count).sum
+      assert_equal 195, Cave.new(EXAMPLE).synchronized_step
+    end
+
+    def test_final
+      assert_equal 1713, Cave.new(Helper.load_input(11)).steps(100).octopuses.map(&:flash_count).sum
+      assert_equal 502, Cave.new(Helper.load_input(11)).synchronized_step
+    end
+  end
+end
