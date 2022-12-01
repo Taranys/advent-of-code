32e1e7b52d4a318fb54a5f6495bbd8c4a689faf8
Yoann Prot <Taranys@users.noreply.github.com>
2022-12-01 12:35:06 +0100

    switch to 2022 (#16)

---
+++ lib/2021/advent_of_code/day21.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module AdventOfCode
+  class Player
+    attr_reader :score
+
+    def initialize(position)
+      @position = position
+      @score = 0
+    end
+
+    def move(value)
+      @position = ((@position - 1) + value) % 10 + 1
+      @score += @position
+    end
+
+  end
+
+  class Dice
+    attr_reader :roll_count
+
+    def initialize
+      @current_value = 0
+      @roll_count = 0
+    end
+
+    def roll
+      @current_value += 1
+      @roll_count += 1
+
+      @current_value = 1 if @current_value == 101
+
+      @current_value
+    end
+  end
+end
