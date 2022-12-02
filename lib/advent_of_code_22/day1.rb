128c75447df4dc2155458ece521b5c0a7126e55c
Yoann Prot <yoann.prot@doctolib.com>
2022-12-02 11:27:53 +0100

    fix rubocop

--- lib/advent_of_code_22/day1.rb
+++ lib/advent_of_code_22/day1.rb
@@ -3,17 +3,22 @@
 module AdventOfCode22
   # Day 1 of the advent of code 2021
   class Day1
-    def self.elves(input)
+    def parse(input)
       input
         .map(&:to_i)
-        .reduce([]) do |line, values|
-          if line.zero?
-            values << 0
-          else
-            values[-1] = values[-1] + current
-          end
+        .each_with_object([0]) do |calories, values|
+        if calories.zero?
+          values << 0
+        else
+          values[-1] += calories
         end
-        .sort.reverse
+      end
+    end
+
+    def self.elves(input)
+      parse(input)
+        .sort
+        .reverse
     end
   end
 end
