d53b646e948fdac818b565bbda61f6b25c1a7d8d
Yoann Prot <Taranys@users.noreply.github.com>
2022-12-04 21:07:02 +0100

    solution for 2022 04 (#21)
    * solution for 2022 04
    
    * fix linter

---
+++ lib/advent_of_code_22/day4.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module AdventOfCode22
+  module Day4
+    class Parser
+      def self.section_assignments(input)
+        input.map do |line|
+          s1, s2 = line.split(",").map { |value| value.split("-") }
+
+          SectionAssignment.new(
+            (s1[0]..s1[1]).to_a,
+            (s2[0]..s2[1]).to_a
+          )
+        end
+      end
+    end
+
+    class SectionAssignment
+      def initialize(assignment1, assignment2)
+        @assignment1 = assignment1
+        @assignment2 = assignment2
+      end
+
+      def fully_overlapse?
+        overlapse.size == @assignment1.size || overlapse.size == @assignment2.size
+      end
+
+      def overlapse?
+        !overlapse.empty?
+      end
+
+      private
+
+      def overlapse
+        @assignment1.intersection(@assignment2)
+      end
+    end
+  end
+end
