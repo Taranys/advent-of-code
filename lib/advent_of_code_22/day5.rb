f05bf7ac99bc35f0d220206c4f8448df654a3911
Yoann Prot <yoann.prot@doctolib.com>
2022-12-05 14:07:17 +0100

    first star

--- lib/advent_of_code_22/day5.rb
+++ lib/advent_of_code_22/day5.rb
@@ -3,7 +3,76 @@
 module AdventOfCode22
   module Day5
     class Parser
+      def self.ship(input)
+        ship_input = input.split("\n\n").first
+        lines = ship_input.split("\n").reject(&:empty?)
+        lines.pop
 
+        ship = Ship.new
+        lines
+          .map do |line|
+            r = []
+            line.chars.each_slice(4) do |v|
+              r << v[1]
+            end
+            r
+        end
+          .reverse
+          .each do |line|
+            line.each_with_index { |value, index| ship.add_crate(index, value) if value != ' ' }
+          end
+
+        ship
+      end
+
+      def self.moves(input)
+        moves_input = input.split("\n\n")[1]
+        lines = moves_input.split("\n").map(&:strip).reject(&:empty?)
+
+        lines
+          .map { |line| /move (.+) from (.) to (.)/.match(line) }
+          .map { |match| Move.new(match[1], match[2], match[3]) }
+      end
+
+    end
+
+    class Ship
+      attr_reader :crates
+      def initialize
+        @crates = []
+      end
+
+      def add_crate(index, value)
+        @crates[index] = [] if @crates[index].nil?
+        @crates[index] << value
+      end
+
+      def top_crates
+        @crates.map { |a| a.last }.join
+      end
+    end
+
+    class Move
+      attr_accessor :count, :from, :to
+      def initialize(count, from_index, to_index)
+        @count = count.to_i
+        @from = from_index.to_i
+        @to = to_index.to_i
+      end
+    end
+
+    class Crane
+      def initialize(ship)
+        @ship = ship
+      end
+
+      def apply(move)
+        move.count.times do
+          crate = @ship.crates[move.from - 1].pop
+          @ship.crates[move.to - 1] << crate
+        end
+        p @ship.top_crates
+      end
     end
   end
 end
--- test/advent_of_code_22/day5/day5_test.rb
+++ test/advent_of_code_22/day5/day5_test.rb
@@ -3,10 +3,10 @@
 require "minitest/autorun"
 
 require_relative "../helper"
-require_relative "../../../lib/advent_of_code_22/day4"
+require_relative "../../../lib/advent_of_code_22/day5"
 
 module AdventOfCode22
-  module Day4
+  module Day5
     class Test < Minitest::Test
       EXAMPLE = %(
     [D]
@@ -18,15 +18,29 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2
-).split("\n").map(&:strip).reject(&:empty?)
+)
+
+      def test_parse_ship
+        assert_equal 'NDP', Parser.ship(EXAMPLE).top_crates
+      end
 
       def test_example
-        assert_equal 2, Parser.section_assignments(EXAMPLE).count(&:fully_overlapse?)
+        ship = Parser.ship(EXAMPLE)
+        moves = Parser.moves(EXAMPLE)
+        crane = Crane.new(ship)
+        moves.each { |move| crane.apply(move) }
+
+        assert_equal 'CMZ', ship.top_crates
       end
 
       def test_first_star
-        input = AdventOfCode22::Helper.load_input(4)
-        assert_equal 657, Parser.section_assignments(input).count(&:fully_overlapse?)
+        input = AdventOfCode22::Helper.load_raw(5)
+        ship = Parser.ship(input)
+        moves = Parser.moves(input)
+        crane = Crane.new(ship)
+        moves.each { |move| crane.apply(move) }
+
+        assert_equal 'RLFNRTNFB', ship.top_crates
       end
 
       def test_second_example
