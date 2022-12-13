3f75cc197495465eba722f6c4930c966e240558f
Taranys <yoann.prot@doctolib.com>
2022-12-13 16:30:06 +0100

    clean up

--- lib/advent_of_code_22/day13.rb
+++ lib/advent_of_code_22/day13.rb
@@ -16,7 +16,6 @@ module AdventOfCode22
 
     class Compare
       def self.pair(v1, v2)
-        p [v1, v2]
         if v1.is_a?(Integer) && v2.is_a?(Integer)
           return :valid if v1 < v2
           return :invalid if v1 > v2
@@ -37,53 +36,6 @@ module AdventOfCode22
         v1.size == v2.size ? :continue : :valid
       end
 
-      def value(v1, v2)
-        return :valid if v1 < v2
-        return :invalid if v1 > v2
-        :continue
-      end
-    end
-
-    class Pair
-      def initialize(first, second)
-        @first = first
-        @second = second
-      end
-
-      def valid?
-        @first.size.times do |index|
-          v1 = @first[index]
-          v2 = @second[index]
-
-          p [v1, v2]
-
-          next if v1.nil?
-          return false if v1.is_a?(Integer) && v2.nil?
-          return false if v1.is_a?(Array) && v2.nil?
-
-          if v1.is_a?(Integer) && v2.is_a?(Integer)
-            return false if v1 > v2
-            return true if v1 < v2
-            next
-          end
-
-          if v1.is_a?(Array) && v2.is_a?(Array)
-            return false unless Pair.new(v1, v2).valid?
-          end
-
-          if !v1.is_a?(Array) && v2.is_a?(Array)
-            return false unless Pair.new([v1], [v2.first]).valid?
-          end
-
-          if v1.is_a?(Array) && !v2.is_a?(Array)
-            return false unless Pair.new([v1.first], [v2]).valid?
-          end
-
-          # p [v1, v2]
-        end
-
-        true
-      end
     end
   end
 end
