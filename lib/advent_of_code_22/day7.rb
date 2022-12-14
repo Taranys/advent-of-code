3c869c3af0b0ab7ce7e5aa9435ab1e7813b568a5
Yoann Prot <Taranys@users.noreply.github.com>
2022-12-14 21:45:09 +0100

    2022 day 07 (#24)
    * complete solution
    
    * fix lint

---
+++ lib/advent_of_code_22/day7.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module AdventOfCode22
+  module Day7
+    class Parser
+      def self.root(input)
+        root = Dir.new(nil, "/")
+        current = root
+
+        input.each do |line|
+          next if line == "$ cd /"
+          next if line == "$ ls"
+
+          if line == "$ cd .."
+            current = current.parent
+            next
+          end
+
+          new_dir_matcher = /dir (.*)/.match(line)
+          current.add_child(Dir.new(current, new_dir_matcher[1])) unless new_dir_matcher.nil?
+
+          new_file_matcher = /(\d+) (.*)/.match(line)
+          unless new_file_matcher.nil?
+            current.add_child(File.new(current, new_file_matcher[2],
+                                       new_file_matcher[1].to_i))
+          end
+
+          go_inside_matcher = /[$] cd (.*)/.match(line)
+          current = current.child(go_inside_matcher[1]) unless go_inside_matcher.nil?
+        end
+
+        root
+      end
+    end
+
+    class Dir
+      attr_reader :parent, :name
+
+      def initialize(parent, name)
+        @parent = parent
+        @name = name
+        @children = []
+      end
+
+      def child(name)
+        @children.find { |child| child.name == name }
+      end
+
+      def add_child(child)
+        @children << child
+      end
+
+      def all_directories(entries = [])
+        @children.each do |child|
+          next if child.is_a? File
+
+          child.all_directories entries
+        end
+        entries << self
+        entries
+      end
+
+      def size
+        @children.map(&:size).sum
+      end
+    end
+
+    class File
+      attr_reader :parent, :name, :size
+
+      def initialize(parent, name, size)
+        @parent = parent
+        @name = name
+        @size = size
+      end
+    end
+  end
+end
---
+++ test/advent_of_code_22/day7/day7_test.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+require_relative "../helper"
+require_relative "../../../lib/advent_of_code_22/day7"
+
+module AdventOfCode22
+  module Day7
+    class Test < Minitest::Test
+      EXAMPLE = %(
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+).split("\n").map(&:strip).reject(&:empty?)
+
+      def test_example
+        assert_equal 95_437, Parser.root(EXAMPLE).all_directories.map(&:size).reject { |size| size > 100_000 }.sum
+      end
+
+      def test_first_star
+        input = AdventOfCode22::Helper.load_input(7)
+        assert_equal 1_513_699, Parser.root(input).all_directories.map(&:size).reject { |size| size > 100_000 }.sum
+      end
+
+      def test_second_example
+        file_system = Parser.root(EXAMPLE)
+        current_available_space = 70_000_000 - file_system.size
+        needed_space = 30_000_000 - current_available_space
+
+        assert_equal 24_933_642, file_system.all_directories.map(&:size).sort.reject { |size|
+                                   size < needed_space
+                                 }.first
+      end
+
+      def test_second_star
+        input = AdventOfCode22::Helper.load_input(7)
+        file_system = Parser.root(input)
+        current_available_space = 70_000_000 - file_system.size
+        needed_space = 30_000_000 - current_available_space
+
+        assert_equal 7_991_939, file_system.all_directories.map(&:size).sort.reject { |size| size < needed_space }.first
+      end
+    end
+  end
+end
